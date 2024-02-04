341cd1ed760f39337e385e3df3a05a2f9af33acf
83319 <fatmaalghannam324@gmail.com>
2024-02-04 15:44:16 +0300

    second Task

---
+++ src/main/java/com/letcode/SecureBankSystem/bo/CreateFarewellRequest.java
@@ -0,0 +1,15 @@
+package com.letcode.SecureBankSystem.bo;
+
+public class CreateFarewellRequest {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+//for now we dont need it
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+
+
+}
---
+++ src/main/java/com/letcode/SecureBankSystem/controller/CodedController.java
@@ -0,0 +1,67 @@
+package com.letcode.SecureBankSystem.controller;
+
+import com.letcode.SecureBankSystem.bo.CreateFarewellRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+//import javax.naming.Name;
+
+@RestController
+
+public class CodedController {
+    private List<Contact>contacts =new ArrayList<>();
+
+    @GetMapping("/sayHI")
+
+    public String sayHi() {
+        return "Welcome to coded";
+    }
+
+    //for new -> open we should do it all crtlC-ctrlV
+    @GetMapping("/greet")
+
+    public String greet(@RequestParam String name) {
+        return "Hi " + name + "!";
+    }
+
+    @PostMapping("/farewell")
+    public String farewell(@RequestBody CreateFarewellRequest createFarewellRequest) {
+        return "GoodBye , " + createFarewellRequest.getName();
+
+    }
+
+    @PostMapping("/addContact")
+    public ResponseEntity<String> addContact(@RequestBody Contact contact) {
+        if (contactExits(contact.getName())){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Contact is already exists");
+
+        }
+        contacts.add(contact);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Contact added successfully ");
+
+
+    }
+    private boolean contactExits(String name){
+        return contacts.stream().anyMatch(c->c.getName().equals(name));
+    }
+
+    @GetMapping("/getContactDetails")
+    public ResponseEntity<Object> getContactDetails(@RequestParam String name) {
+        Optional<Contact> foundContact = contacts.stream()
+                .filter(c -> c.getName().equals(name))
+                .findFirst();
+
+        if (foundContact.isPresent()) {
+            return ResponseEntity.ok(foundContact.get());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Contact not found.");
+        }
+    }
+
+
+}
